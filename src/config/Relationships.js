2d7b35ac1f35be8c5374bc7582bfa63a9d987dc1
fodaymans <30390029+fodaymans@users.noreply.github.com>
2022-08-04 10:05:56 +0000

    Add files via upload

---
+++ src/config/Relationships.js
@@ -0,0 +1,30 @@
+const categories = [
+    { label:"Daughter", value: "Daughter" },
+    { label: "Son", value: "Son" },
+    { label: "Wife", value:"Wife" },
+    { label: "Husband", value: "Husband" },
+    { label: "Mother", value: "Mother" },
+    { label: "Father", value: "Father" },
+    { label: "Sister", value: "Sister" },
+
+    { label: "Brother", value: "Brother" },
+    { label: "Niece", value: "Niece" },
+    { label: "Nephew", value: "Nephew" },
+    { label: "Cousin", value: "Cousin" },
+    { label: "Aunt", value: "Aunt" },
+
+    { label: "Uncle", value: "Uncle" },
+    { label: "Grand-Mother", value: "Grand-Mother" },
+    { label: "Grand-Father", value: "Grand-Father" },
+
+
+  
+   
+     
+
+
+  ];
+
+
+
+  export default categories;
